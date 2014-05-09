0f377fb43749e198ad8b3814f3c3f560ae37f572
Funwayguy <FunwayguyEmail@gmail.com>
2014-05-09 13:13:03 +1000

    MAJOR PHYSICS PERFORMANCE FIX PART 3 Fixed physics acting way too slowly Moved physics configs to dedicated category New config for physics speed New config for world start delay on physics

--- src/main/java/org/randomcoders/economy/handlers/packets/PacketEconomy.java
+++ src/main/java/org/randomcoders/economy/handlers/packets/PacketEconomy.java
@@ -3,7 +3,17 @@ package org.randomcoders.economy.handlers.packets;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import org.randomcoders.economy.handlers.trading.HandlerEconomy;
+import org.randomcoders.economy.handlers.trading.ItemInfo;
+import org.randomcoders.economy.inventory.GuiTrader;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.packet.Packet250CustomPayload;
+import net.minecraft.server.MinecraftServer;
 
 public class PacketEconomy
 {
@@ -18,20 +28,77 @@ public class PacketEconomy
 			
 			switch(actionID)
 			{
-				case 0: // Get ItemStack information
+				case 0: // Request ItemInfo (process this serverside)
 				{
+					int dbIndex = inputStream.readInt();
+					int itemID = inputStream.readInt();
+					int entityID = inputStream.readInt();
+					int worldDim = inputStream.readInt();
+					
+					if(MinecraftServer.getServer().isServerRunning())
+					{
+						Entity entity = MinecraftServer.getServer().worldServers[worldDim].getEntityByID(entityID);
+						if(entity instanceof EntityPlayer)
+						{
+							EntityPlayer player = (EntityPlayer)entity;
+							
+							ItemInfo iInfo = null;
+							
+							if(dbIndex == 0)
+							{
+								iInfo = HandlerEconomy.economyDB.get(itemID);
+							} else if(dbIndex == 1)
+							{
+								iInfo = HandlerEconomy.enchantDB.get(itemID);
+							}
+							
+							if(iInfo == null)
+							{
+								iInfo = new ItemInfo(itemID, -1);
+							}
+						}
+					}
+					
 					break;
 				}
-				case 1: // Get next n Economy entries
+				
+				case 1: // ItemInfo response (process this clientside)
 				{
+					int dbIndex = inputStream.readInt();
+					int entityID = inputStream.readInt();
+					ObjectInputStream objIn = new ObjectInputStream(inputStream);
+					HashMap<String, HashMap> rawInfo = (HashMap<String, HashMap>)objIn.readObject();
 					
+					ItemInfo iInfo = rebuildInfo(rawInfo);
+					
+					GuiScreen curScreen = Minecraft.getMinecraft().currentScreen;
+					
+					if(curScreen != null && curScreen instanceof GuiTrader && iInfo != null)
+					{
+						GuiTrader trader = (GuiTrader)curScreen;
+						
+						if(dbIndex == 0)
+						{
+							trader.tmpEconDB.put(iInfo.itemID, iInfo);
+						} else if(dbIndex == 1)
+						{
+							trader.tmpEnchDB.put(iInfo.itemID, iInfo);
+						}
+					}
+					objIn.close();
+					break;
 				}
 			}
 
 			inputStream.close();
-		} catch(IOException e)
+		} catch(IOException | ClassNotFoundException e)
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	public static ItemInfo rebuildInfo(HashMap<String, HashMap> entry)
+	{
+		return null;
 	}
 }
